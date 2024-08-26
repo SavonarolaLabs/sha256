c482627f94254a94e52cbe0807e5aba68cedb6f3
c8e4 <c8e4@proton.me>
2024-08-26 11:14:18 +0200

    h, k constants

--- index.js
+++ index.js
@@ -80,4 +80,44 @@ function textToHexEditorFormat(txt) {
 	return result.trim();
 }
 
+function calculateConstants() {
+	const primes = [
+		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+		71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139,
+		149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
+		227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,
+		307, 311, 313,
+	];
+
+	const h = primes
+		.slice(0, 8)
+		.map((p) => Math.floor((Math.sqrt(p) % 1) * 2 ** 32).toString(16));
+
+	const k = primes
+		.slice(0, 64)
+		.map((p) => Math.floor((Math.cbrt(p) % 1) * 2 ** 32).toString(16));
+
+	return { h, k };
+}
+
 console.log(textToHexEditorFormat(text));
+
+const { h, k } = calculateConstants();
+
+console.log(
+	"\nh Constants:\n" +
+		h
+			.map((val, i) => `0x${val}`)
+			.reduce((acc, val, i) => {
+				return acc + val + (i % 4 === 3 ? "\n" : "\t");
+			}, "")
+);
+
+console.log(
+	"\nk Constants:\n" +
+		k
+			.map((val, i) => `0x${val}`)
+			.reduce((acc, val, i) => {
+				return acc + val + (i % 4 === 3 ? "\n" : "\t");
+			}, "")
+);
